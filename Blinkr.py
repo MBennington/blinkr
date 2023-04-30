71eee3fbb972a50017a6bd7f714132a8f448e112
-MADUKA- <57235170+MBennington@users.noreply.github.com>
2023-04-30 23:33:34 +0530

    blinkr Initial commit

---
+++ Blinkr.py
@@ -0,0 +1,104 @@
+import cv2
+import cvzone
+
+from cvzone.FaceMeshModule import FaceMeshDetector
+from cvzone.PlotModule import LivePlot
+import pyautogui
+import pygame
+import time
+
+# initialize Pygame
+pygame.init()
+
+# load the image
+image = pygame.image.load("loader.png")
+
+# create the splash screen
+splash = pygame.Surface((747, 497))
+splash.fill((0, 46, 65))  # set background color
+splash.blit(image, (0, 0))  # draw the image
+
+# create the main window without borders
+screen = pygame.display.set_mode((747, 497), pygame.NOFRAME)
+screen.blit(splash, (0, 0))  # draw the splash screen on the main window
+pygame.display.flip()  # update the display
+
+# show the splash screen for a few seconds
+time.sleep(5)
+
+# close Pygame
+pygame.quit()
+
+
+cap = cv2.VideoCapture(0)
+detector = FaceMeshDetector(maxFaces=4)
+plotY = LivePlot(640, 360, [20, 50], invert=True)
+
+idList = [22, 23, 24, 26, 110, 157, 158, 159, 160, 161, 130, 243]
+ratioList = []
+blinkCounter = 0
+counter = 0
+color = (255, 0, 255)
+no_blink_time = 0  # initialize time since last blink to 0
+notification_visible = False  # initialize notification as not visible
+
+while True:
+
+    if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
+        cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+
+    success, img = cap.read()
+    img, faces = detector.findFaceMesh(img, draw=False)
+
+    if faces:
+        face = faces[0]
+        for id in idList:
+            cv2.circle(img, face[id], 5,color, cv2.FILLED)
+
+        leftUp = face[159]
+        leftDown = face[23]
+        leftLeft = face[130]
+        leftRight = face[243]
+        lenghtVer, _ = detector.findDistance(leftUp, leftDown)
+        lenghtHor, _ = detector.findDistance(leftLeft, leftRight)
+
+        cv2.line(img, leftUp, leftDown, (0, 200, 0), 3)
+        cv2.line(img, leftLeft, leftRight, (0, 200, 0), 3)
+
+        ratio = int((lenghtVer / lenghtHor) * 100)
+        ratioList.append(ratio)
+        if len(ratioList) > 3:
+            ratioList.pop(0)
+        ratioAvg = sum(ratioList) / len(ratioList)
+
+        if ratioAvg < 35 and counter == 0:
+            blinkCounter += 1
+            color = (0,200,0)
+            counter = 1
+            no_blink_time = 0  # reset time since last blink
+            if notification_visible:
+                cv2.destroyWindow("Notification")  # close the notification window
+                notification_visible = False
+        else:
+            no_blink_time += 1  # increment time since last blink
+            
+
+
+        if counter != 0:
+            counter += 1
+            if counter > 10:
+                counter = 0
+                color = (255,0, 255)
+
+        cvzone.putTextRect(img, f'Blink Count: {blinkCounter}', (50, 100),
+                           colorR=color)
+
+        imgPlot = plotY.update(ratioAvg, color)
+        img = cv2.resize(img, (640, 360))
+        imgStack = cvzone.stackImages([img, imgPlot], 2, 1)
+    else:
+        img = cv2.resize(img, (640, 360))
+        imgStack = cvzone.stackImages([img, img], 2, 1)
+
+    cv2.imshow("Image", imgStack)
+    cv2.waitKey(25)
